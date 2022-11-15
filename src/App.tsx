ebd5dc7d42f2750510e7b228ca2bc124df25ba44
Shrimptrimp <kasperkukkonen08@gmail.com>
2022-11-15 14:32:08 +0200

    IT WORKS

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import MyFirstComponent from './components/myFirstComponent';
 import SomeExampleComponent from './components/example/someExampleComponent';
 import ClickCount from './components/example/clickCount';
-import GooseGif from './components/example/gooseGif';
 
 const App: React.FC = () => {
   return (
