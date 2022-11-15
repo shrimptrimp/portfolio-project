28838e955eb19950d71f4a8a8a7faea1875a8352
Shrimptrimp <kasperkukkonen08@gmail.com>
2022-11-15 15:04:02 +0200

    added style file for index

---
+++ src/components/example/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import App from '../../App';
+import reportWebVitals from '../../reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import MyFirstComponent from '../myFirstComponent';
+import SomeExampleComponent from './someExampleComponent';
+
+<React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}></Route>
+        <Route path='example' element={<SomeExampleComponent />} />
+        <Route path='/first' element={<MyFirstComponent />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
