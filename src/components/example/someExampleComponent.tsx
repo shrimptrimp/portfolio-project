f2cb8299c244f79fa798a3ca87346f0af7c1e3aa
Shrimptrimp <kasperkukkonen08@gmail.com>
2022-11-15 12:18:47 +0200

    fixed snippet issue

---
+++ src/components/example/someExampleComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type SomeExampleComponentProps = {
+
+}
+
+const SomeExampleComponent: React.FC<SomeExampleComponentProps> = () => {
+  return (
+	<div>
+	  SomeExampleComponent
+	</div>
+  )
+}
+
+export default SomeExampleComponent
