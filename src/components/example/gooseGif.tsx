b8fc0b3ffa4e7574d922034a961b1d0e660f4873
Shrimptrimp <kasperkukkonen08@gmail.com>
2022-11-15 13:56:26 +0200

    asdg

---
+++ src/components/example/gooseGif.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type GooseGifProps = {
+
+}
+
+const GooseGif: React.FC<GooseGifProps> = () => {
+  return (
+    <div>
+      <body>
+        <img src="https://tenor.com/view/dance-swan-pigeon-goose-gif-16042892" alt="geese" />
+      </body>
+    </div>
+  )
+}
+
+export default GooseGif
