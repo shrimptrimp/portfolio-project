5243fd372b475399bff0077e9142b4f4595e358e
Shrimptrimp <kasperkukkonen08@gmail.com>
2022-11-15 14:56:09 +0200

    added router-dom components

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
