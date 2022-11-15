cf33c9abb35611d2a608a28bb8e2d437dc4092c4
Shrimptrimp <kasperkukkonen08@gmail.com>
2022-11-15 13:53:36 +0200

    trying yet again, but as a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import MyFirstComponent from './components/myFirstComponent';
 import SomeExampleComponent from './components/example/someExampleComponent';
 import ClickCount from './components/example/clickCount';
+import GooseGif from './components/example/gooseGif';
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <MyFirstComponent />
       <SomeExampleComponent />
       <ClickCount />
+      <GooseGif />
     </div>
   )
 }
