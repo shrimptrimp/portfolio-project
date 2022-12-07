226be82ca6a08ed7af56b41c4d133c966cef8450
Shrimptrimp <kasperkukkonen08@gmail.com>
2022-12-07 10:43:22 +0200

    npm issue fixed, began work on the navbar

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './navbar.module.scss'
+type NavbarProps = {
+
+}
+
+const Navbar: React.FC<NavbarProps> = () => {
+  return (
+    <div>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar
+
---
+++ src/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import MyFirstComponent from './components/myFirstComponent';
+import SomeExampleComponent from './components/example/someExampleComponent';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+const Index: React.FC = () => {
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />}></Route>
+          <Route path='example' element={<SomeExampleComponent />} />
+          <Route path='/first' element={<MyFirstComponent />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  )
+}
+
+root.render(<Index />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
