a2269df8dc6047b6738c9766da5d4b7868cb57db
Shrimptrimp <kasperkukkonen08@gmail.com>
2022-12-08 11:31:50 +0200

    added some styling

--- src/components/example/main.tsx
+++ src/components/example/main.tsx
@@ -7,11 +7,13 @@ type mainProps = {
 
 const main: React.FC<mainProps> = () => {
   return (
-      <div className={styles.container}>
+    <div className={styles.container}>
+      <div>
         <h1>Kasper Juhani Kukkonen</h1>
-        <a href='https://github.com/shrimptrimp' target="_blank" rel="noreferrer">Github</a>
+       <a href='https://github.com/shrimptrimp' target="_blank" rel="noreferrer">Github</a>
       </div>
-    
+
+    </div>
   )
 }
 
