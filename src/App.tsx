b28f580659a6f1688a7e60398bca256bb67210a8
shrimptrimp <kasperkukkonen08@gmail.com>
2022-12-12 14:00:14 +0200

    trying yet again

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // App.tsx
 import React from 'react'
-import CV from './CV';
+import CV from './components/CV';
 import Main from './components/example/main';
 
 const App: React.FC = () => {
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import CV from './CV';
+import CV from './components/CV';
 import Main from './components/example/main';
 import ReactDOM from 'react-dom/client';
 import Navbar from './components/navbar/navbar';
