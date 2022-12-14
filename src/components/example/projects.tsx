2d29c9ece94da21926ea3a87a954d1c363e01d18
shrimptrimp <kasperkukkonen08@gmail.com>
2022-12-14 23:45:23 +0200

    added some more stuff to the projects page

--- src/components/example/projects.tsx
+++ src/components/example/projects.tsx
@@ -6,9 +6,14 @@ type ProjectsProps = {
 
 const Projects: React.FC<ProjectsProps> = () => {
   return (
+    <html>
     <div className={styles.container}>  
       <a href='https://play.unity.com/u/Shrimptrimp' target="_blank" rel="noreferrer">Unity Games</a>
+      </div>
+      <div className={styles.container2}>
+      <a href='https://github.com/shrimptrimp/portfolio-project' target="_blank" rel="noreferrer">This Page</a>
     </div>
+    </html>
   )
 }
 
