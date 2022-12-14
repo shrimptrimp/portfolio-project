b419e84e04977929f9b1ccf360616e459cbe06aa
shrimptrimp <kasperkukkonen08@gmail.com>
2022-12-14 23:05:22 +0200

    trying AGAAAAAAAIN

---
+++ src/components/example/projects.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './projects.module.scss'
+type ProjectsProps = {
+
+}
+
+const Projects: React.FC<ProjectsProps> = () => {
+  return (
+    <div className={styles.container}>
+      ababababa
+    </div>
+  )
+}
+
+export default Projects
