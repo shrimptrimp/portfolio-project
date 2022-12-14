ea5642b180edafcf291641ce898e460226adf2f9
shrimptrimp <kasperkukkonen08@gmail.com>
2022-12-14 23:08:59 +0200

    removing it from the main page breaks it :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 // App.tsx
 import React from 'react'
+import CV from './components/example/CV';
 import Main from './components/example/main';
 import Projects from './components/example/projects';
 const App: React.FC = () => {
   return (
     <div>
       <Main />
+      <Projects />
+      <CV />
     </div>
   )
 }
