e5900c14bd880eecd565782a720d2ebf8a464112
shrimptrimp <kasperkukkonen08@gmail.com>
2022-12-14 23:02:23 +0200

    trying AGAAIIIIN

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // App.tsx
 import React from 'react'
 import Main from './components/example/main';
+import Projects from './components/example/projects';
 const App: React.FC = () => {
   return (
     <div>
       <Main />
+      <Projects />
     </div>
   )
 }
