9225fb309af1ac54353e4e6fd35a2244e5bec60d
Shrimptrimp <kasperkukkonen08@gmail.com>
2022-12-15 11:13:39 +0200

    added inline-block effect with display:flex, justify-content: space-evenly and text-align:center ty markus

--- src/components/example/main.tsx
+++ src/components/example/main.tsx
@@ -6,18 +6,19 @@ type mainProps = {
 }
 
 const main: React.FC<mainProps> = () => {
-  return (
+  return ( 
     <div className={styles.container}>
       <div>
         <h1>Kasper Juhani Kukkonen 
         <h2>Shrimptrimp</h2>
         <h2>Aspiring Software Engineer</h2>
         </h1>
-       <a href='https://github.com/shrimptrimp' target="_blank" rel="noreferrer">Github</a>
-       <a href='https://www.linkedin.com/in/kasper-juhani-kukkonen/' target="_blank" rel="noreferrer">LinkedIn</a>
+        <div className={styles.container2}>
+<a href='https://github.com/shrimptrimp' target="_blank" rel="noreferrer">Github</a>    
+<a href='https://www.linkedin.com/in/kasper-juhani-kukkonen/' target="_blank" rel="noreferrer">LinkedIn</a>
+        </div>
+        </div>
       </div>
-
-    </div>
   )
 }
 
