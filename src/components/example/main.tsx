59bb13293e30b62f30d731252465e3812d36bba5
shrimptrimp <kasperkukkonen08@gmail.com>
2022-12-20 03:14:33 +0200

    added some more styling, put contact info front and center

--- src/components/example/main.tsx
+++ src/components/example/main.tsx
@@ -12,8 +12,12 @@ const main: React.FC<mainProps> = () => {
         <h1>Kasper Juhani Kukkonen
           <h2>Shrimptrimp</h2>
           <h2>Aspiring Software Engineer</h2>
+          <h3>Contact me!</h3>
+          <h3>+358449765887</h3>
+          <h3>kasperkukkonen08@gmail.com</h3>
         </h1>
         <div className={styles.container2}>
+          
           <a href='https://github.com/shrimptrimp' target="_blank" rel="noreferrer">Github</a>
           <a href='https://www.linkedin.com/in/kasper-juhani-kukkonen/' target="_blank" rel="noreferrer">LinkedIn</a>
         </div>
